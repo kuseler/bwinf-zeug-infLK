f823b459d2b43237fc9b432879c86608a2e6c46a
kuseler <47039588+kuseler@users.noreply.github.com>
2021-10-06 12:50:23 +0200

    Update genemptyarr.py

--- genemptyarr.py
+++ genemptyarr.py
@@ -11,9 +11,11 @@ def genemptyarr(x,y):
 def integratewordtoarr(direction, xpos, ypos, word, arr):
     if direction <= 4:
         if direction == 1:
-            if xpos + len(word) <= len(arr[ypos]) and checkwordperword(direction, word, xpos, ypos, arr):
+            if xpos + len(word) <= len(arr[ypos]) and not checkwordperword(direction, word, xpos, ypos, arr):
                 for j,i in enumerate(word):
                     arr[ypos][xpos+j] = i
+                    print(i)
+            
         if direction == 2:
             if ypos + len(word) <= len(arr) and checkwordperword(direction, word, xpos, ypos, arr):
                 pass
@@ -26,14 +28,20 @@ def integratewordtoarr(direction, xpos, ypos, word, arr):
 def checkwordperword(direction, word, xpos, ypos, arr):
     if direction == 1:
         for j,i in enumerate(word):
-            if arr[ypos][xpos + j] == word or not arr[ypos][xpos+j]:
+            print(j, arr[ypos][xpos + j], i)
+            if arr[ypos][xpos + j] == i or arr[ypos][xpos+j] == None:
                 pass
             else:
-                return False
+                return True
+        
     if direction == 2:
         for j,i in enumerate(word):
-            if arr[ypos+j][xpos] == word or not arr[ypos][xpos+j]:
+            print(j, arr[ypos + j][xpos], i)
+            if arr[ypos][xpos + j] == i or arr[ypos][xpos] == None:
                 pass
             else:
-                return False
+                return True
+
             
+    if direction == 3:
+        pass
