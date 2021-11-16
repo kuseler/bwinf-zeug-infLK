17b6d36ab7aedd8e24b304278b2e26023b08d4f2
kuseler <47039588+kuseler@users.noreply.github.com>
2021-11-16 11:04:05 +0100

    Update genemptyarr.py

--- genemptyarr.py
+++ genemptyarr.py
@@ -7,41 +7,74 @@ def genemptyarr(x,y):
     #x = np.array(arr)
     return arr
 
+def integrate_horizontally(xpos, ypos, word, arr):
+    if checkwordperword(1, word, xpos, ypos, arr):
+        for position,letter in enumerate(word):
+            arr[ypos][xpos+position] = letter
+            print(letter)
+
+def integrate_vertically(xpos, ypos, word, arr):
+    if checkwordperword(2, word, xpos, ypos, arr):
+        for position, letter in enumerate(word):
+            arr[ypos+position][xpos] = letter
+            print(letter)
+
+
+def integrate_diagonally(xpos, ypos, word, arr):
+    if checkwordperword(3, word, xpos, ypos, arr):
+        for position, letter in enumerate(word):
+            arr[ypos + position][xpos + position] = letter
+            print(letter)
 
 def integratewordtoarr(direction, xpos, ypos, word, arr):
-    if direction <= 4:
-        if direction == 1:
-            if xpos + len(word) <= len(arr[ypos]) and not checkwordperword(direction, word, xpos, ypos, arr):
-                for j,i in enumerate(word):
-                    arr[ypos][xpos+j] = i
-                    print(i)
+    if direction == 1:
+        integrate_horizontally(xpos, ypos, word, arr)
             
-        if direction == 2:
-            if ypos + len(word) <= len(arr) and checkwordperword(direction, word, xpos, ypos, arr):
-                pass
+    if direction == 2:
+        if ypos + len(word) <= len(arr) and checkwordperword(direction, word, xpos, ypos, arr):
+            pass
       
     if direction >= 5:
         word = word[::-1]
         
     
 
+
 def checkwordperword(direction, word, xpos, ypos, arr):
     if direction == 1:
+        if len(word) > len(arr[ypos]):
+            return False
         for j,i in enumerate(word):
-            print(j, arr[ypos][xpos + j], i)
-            if arr[ypos][xpos + j] == i or arr[ypos][xpos+j] == None:
-                pass
-            else:
-                return True
+            next_position = arr[ypos][xpos + j]
+            print(j, next_position, i)
+            if not (next_position == i or next_position == None):
+                return False
+        return True
         
     if direction == 2:
-        for j,i in enumerate(word):
-            print(j, arr[ypos + j][xpos], i)
-            if arr[ypos][xpos + j] == i or arr[ypos][xpos] == None:
+        if len(word) > len(arr):
+            return False
+        for position, letter in enumerate(word):
+            next_position = arr[ypos+position][xpos]
+            print(position, next_position, letter)
+            if not (next_position == letter or next_position == None):
+                return False
+        return True    
+    if direction == 3:
+        if len(word) > len(arr) or len(word) > len(arr[ypos]):
+            return False
+        for position, letter in enumerate(word):
+            next_position = arr[ypos+position][xpos+position]
+            print(position, next_position, letter)
+            if next_position == letter or next_position == None:
                 pass
             else:
-                return True
+                return False
+        return True
 
             
     if direction == 3:
         pass
+
+
+a = genemptyarr(5,5)
