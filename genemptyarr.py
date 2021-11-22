e5cd6f2ea053b81f77f612d976843ad0c53646ec
kuseler <47039588+kuseler@users.noreply.github.com>
2021-11-22 22:37:54 +0100

    Add files via upload

--- genemptyarr.py
+++ genemptyarr.py
@@ -11,51 +11,50 @@ def integrate_horizontally(xpos, ypos, word, arr):
     if checkwordperword(1, word, xpos, ypos, arr):
         for position,letter in enumerate(word):
             arr[ypos][xpos+position] = letter
-            print(letter)
+##            print(letter)
     else:
-        print("integration failed")
+##        print("integration failed")
         return False
 
 def integrate_vertically(xpos, ypos, word, arr):
     if checkwordperword(2, word, xpos, ypos, arr):
         for position, letter in enumerate(word):
             arr[ypos+position][xpos] = letter
-            print(letter)
+##            print(letter)
     else:
-        print("integration failed")
+##        print("integration failed")
         return False
 
 
-def integrate_diagonally(xpos, ypos, word, arr):
-    if checkwordperword(3, word, xpos, ypos, arr):
-        for position, letter in enumerate(word):
-            arr[ypos + position][xpos + position] = letter
-            print(letter)
+def integrate_diagonally(xpos, ypos, word, arr, downwards=True):
+    if downwards:
+        if checkwordperword(3, word, xpos, ypos, arr):
+            for position, letter in enumerate(word):
+                arr[ypos + position][xpos + position] = letter
+##                print(letter)
+        else:
+##            print("integration failed")
+            return False
     else:
-        print("integration failed")
-        return False
+        if checkwordperword(4, word, xpos, ypos, arr):
+            for position, letter in enumerate(word):
+                arr[ypos - position][xpos + position] = letter
+##                print(letter)
+        else:
+##            print("integration failed")
+            return False
 
-def integratewordtoarr(direction, xpos, ypos, word, arr):
-    if direction == 1:
-        integrate_horizontally(xpos, ypos, word, arr)
-            
-    if direction == 2:
-        integrate_vertically(xpos, ypos, word, arr)
-      
-    if direction >= 5:
-        word = word[::-1]
-        
-    
 
 
 def checkwordperword(direction, word, xpos, ypos, arr):
     if direction == 1:
-        print(ypos)
+##        print(ypos,xpos)
         if len(word)+xpos > len(arr[0]):
             return False
         for j,i in enumerate(word):
+##            print(xpos, j, arr[ypos][xpos])
             next_position = arr[ypos][xpos + j]
-            print(j, next_position, i)
+##            print(j, next_position, i)
             if not (next_position == i or next_position == None):
                 return False
         return True
@@ -65,24 +64,30 @@ def checkwordperword(direction, word, xpos, ypos, arr):
             return False
         for position, letter in enumerate(word):
             next_position = arr[ypos+position][xpos]
-            print(position, next_position, letter)
+##            print(position, next_position, letter)
             if not (next_position == letter or next_position == None):
                 return False
         return True    
     if direction == 3:
-        if len(word) > len(arr) or len(word) > len(arr[ypos]):
+        if len(word)+ypos > len(arr) or len(word)+xpos > len(arr[0]):
             return False
         for position, letter in enumerate(word):
             next_position = arr[ypos+position][xpos+position]
-            print(position, next_position, letter)
+##            print(position, next_position, letter)
             if next_position == letter or next_position == None:
                 pass
             else:
                 return False
         return True
-
-            
-    if direction == 3:
-        pass
-
-
+    if direction == 4:
+        if ypos - (len(word)-1) < 0 or len(word)+xpos > len(arr[0]):
+            return False
+        for position, letter in enumerate(word):
+##            print(position, ypos-position,xpos+position, letter)
+            next_position = arr[ypos-position][xpos+position]
+            if next_position == letter or next_position == None:
+                pass
+            else:
+                return False
+        return True
+        
--- main.py
+++ main.py
@@ -1,14 +1,85 @@
 import genemptyarr
 import random
 text = """
-6 6
-6
-EIN
-DA
-ER
+40 32
+77
+ABRUFDATUM
+ABSATZ
+ACHTUNG
+ALLMUSIC
+ARCHIV
+ARCHIVBOT
+ARCHIVIERUNG
+AUS
+AUT
+AUTOARCHIV
+BABEL
+BAUSTELLE
+BBKL
+BEGRIFFSKLÄRUNG
+BEGRIFFSKLÄRUNGSHINWEIS
+BEL
+BELEGE
+BENUTZER
+BGR
+BIBRECORD
+BOOLAND
+CAN
+CENTER
+CHARTS
+COL
+COMMONS
+COMMONSCAT
+COORDINATE
+COORDINATEMAP
+DDB
+DEU
+DISKUSSIONSSEITE
+DOI
+ERLEDIGT
+FARBLEGENDE
+FILM
+FN
+FNZ
+FOLGENLEISTE
+FRA
+FUSSBALLDATEN
+GEOQUELLE
+GER
+GNIS
+HÖHE
+IMDB
 INFO
-DU
-UND
+INFORMATION
+INTERNETQUELLE
+IPA
+KALENDERSTIL
+KASTEN
+KATEGORIEGRAPH
+LANG
+LITERATUR
+LIZENZUMSTELLUNG
+MEDAILLENSPIEGEL
+MULTILINGUAL
+MUSIKCHARTS
+NAVFRAME
+NAVIGATIONSLEISTE
+NOCOMMONS
+ÖSTERREICHBEZOGEN
+PERSONENLEISTE
+PING
+POSITIONSKARTE
+PRO
+SMILEY
+SORT
+TAXOBOX
+TEXT
+WAPPENRECHT
+WEBARCHIV
+WIKIDATA
+WIKISOURCE
+WIKTIONARY
+ZITATION
 """
 liste = text.split("\n")
 liste = [ i for i in liste if i]
@@ -16,31 +87,127 @@ x,y = int(liste[0].split()[0]), int(liste[0].split()[1])
 rechteck = genemptyarr.genemptyarr(x,y)
 wortliste = liste[2:]
 
+def random_number(lower, upper):
+    if upper < 0:
+        return False
+    else:
+        return random.randint(lower, upper)
+
+def fill_random(array):
+    for y_iter,q in enumerate(array):
+        for x_iter,p in enumerate(array[y_iter]):
+            if not array[y_iter][x_iter]:
+                array[y_iter][x_iter]= random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    return array
+
 def fill_1(array, wortliste):
     remaining = wortliste
     for i in remaining:
         while True:
             if random.randint(0,1) == 0:
-                x_word,y_word = random.randint(0, x-len(i)-1), random.randint(0,y-1)
+                x_word,y_word = random_number(0, x-len(i)-1), random_number(0,y-1)
                 if genemptyarr.integrate_horizontally(x_word, y_word, i, array) != False:
-                    print(i, x_word, y_word)
+##                    print(i, x_word, y_word)
                     break
                 else:
                     pass
             else:
-                x_word,y_word = random.randint(0, x-1), random.randint(0,y-len(i)-1)
-                if genemptyarr.integrate_horizontally(x_word, y_word, i, array) != False:
-                    print(i, x_word, y_word)
+                x_word,y_word = random_number(0, x-1), random_number(0,y-len(i)-1)
+                if genemptyarr.integrate_vertically(x_word, y_word, i, array) != False:
+##                    print(i, x_word, y_word)
                     break
                 else:
                     pass
-    for y_iter,q in enumerate(array):
-        for x_iter,p in enumerate(array[y_iter]):
-            if not array[y_iter][x_iter]:
-                array[y_iter][x_iter]= random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-                print(i)
+    fill_random(array)
     return array
             
-        
 
-fill_1(rechteck, wortliste)
+def fill_2(array, wortliste):
+    remaining = wortliste
+    for i in remaining:
+        while True:
+            chosen = random.randint(0,2)
+            if chosen == 0:
+                x_word,y_word = random_number(0, x-len(i)-1), random_number(0,y-1)
+                if genemptyarr.integrate_horizontally(x_word, y_word, i, array) != False:
+                    #print(i, x_word, y_word)
+                    break
+                else:
+                    pass
+            elif chosen == 1:
+                x_word,y_word = random_number(0, x-1), random_number(0,y-len(i)-1)
+                if genemptyarr.integrate_vertically(x_word, y_word, i, array) != False:
+                    #print(i, x_word, y_word)
+                    break
+                else:
+                    pass
+            else:
+                if random.randint(0,1) == 0:
+                    x_word,y_word = random_number(0, x-len(i)-1), random_number(0,y-len(i)-1)
+                    if genemptyarr.integrate_diagonally(x_word, y_word, i, array) != False:
+                        #print(i, x_word, y_word)
+                        break
+                    else:
+                        pass
+                else:
+                    x_word,y_word = random_number(0, x-len(i)-1), random_number(0,y-len(i)-1)
+                    if genemptyarr.integrate_diagonally(x_word, y_word, i, array, False) != False:
+                        #print(i, x_word, y_word)
+                        break
+                    else:
+                        pass
+                    
+    fill_random(array)
+    return array
+                
+
+
+def fill_3(array, wortliste):
+    remaining = wortliste
+    for i in remaining:
+        while True:
+            chosen = random.randint(0,7)
+            if chosen >= 4:
+                i = i[::-1]
+                chosen -= 4
+                if chosen == 0:
+                    x_word,y_word = random_number(0, x-len(i)-1), random_number(0,y-1)
+                    if genemptyarr.integrate_horizontally(x_word, y_word, i, array) != False:
+                        #print(i, x_word, y_word)
+                        break
+                    else:
+                        pass
+                elif chosen == 1:
+                    x_word,y_word = random_number(0, x-1), random_number(0,y-len(i)-1)
+                    if genemptyarr.integrate_vertically(x_word, y_word, i, array) != False:
+                        #print(i, x_word, y_word)
+                        break
+                    else:
+                        pass
+                else:
+                    if random.randint(0,1) == 0:
+                        x_word,y_word = random_number(0, x-len(i)-1), random_number(0,y-len(i)-1)
+                        if genemptyarr.integrate_diagonally(x_word, y_word, i, array) != False:
+                            #print(i, x_word, y_word)
+                            break
+                        else:
+                            pass
+                    else:
+                        x_word,y_word = random_number(0, x-len(i)-1), random_number(0,y-len(i)-1)
+                        if genemptyarr.integrate_diagonally(x_word, y_word, i, array, False) != False:
+                            #print(i, x_word, y_word)
+                            break
+                        else:
+                            pass
+    fill_random(array)
+    return array
+
+
+
+
+
+            
+fill_3(rechteck, wortliste)
+for i in rechteck:
+    print(i)
+    
